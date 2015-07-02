8abce737f6639abed75ac1af70a973bdda97f7da
lholiday <larrycholiday@gmail.com>
2015-07-02 15:32:54 -0600

    add some test code

---
+++ testTutConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testTutConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+            Console.WriteLine("Press any key.");
+            Console.ReadKey();
+        }
+    }
+}
